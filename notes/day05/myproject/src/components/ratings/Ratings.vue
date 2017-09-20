626ed14fd61c050d169bfd96cd261773540d9d4a
liaozq009 <245822113@qq.com>
2017-09-20 17:43:49 +0800

    vue

---
+++ notes/day05/myproject/src/components/ratings/Ratings.vue
@@ -0,0 +1,45 @@
+<template>
+  <!-- 注意：所有内容都放在一个div里面 -->
+  <div class="ratings">
+      这是ratings页面
+      <ul>
+        <li v-for="item in ratings">{{item.text}}</li>
+      </ul>
+  </div>
+
+</template>
+
+<script>
+  // 获取vuex的数据
+  import {mapGetters} from 'vuex'
+
+  export default{
+    created(){
+      this.$store.dispatch('getRatings'); //页面创建的时候开始发送请求
+    },
+    computed:{
+      ...mapGetters([ //获取vuex的数据
+        'ratings'
+      ]),
+      msg(){
+        return 'welcome to my liaozhangquan';
+      }
+    }
+  }
+
+</script>
+
+<style lang="less" scoped>
+  .header{
+    height: 134px;
+    background-color: rgba(7,17,27,0.1);
+    font-size: 14px;
+    img{
+      width: 64px;
+      height:64px;
+      border-radius: 2px;
+    }
+  }
+
+</style>
+
--- notes/day05/myproject/src/router/index.js
+++ notes/day05/myproject/src/router/index.js
@@ -1,10 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Goods from '../components/goods/Goods.vue'
+import Ratings from '../components/ratings/Ratings.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    
-  ]
+    {
+    	path:'/goods',
+    	component:Goods
+	},
+    {
+    	path:'/ratings',
+    	component:Ratings
+	},
+	{
+		path:'*',
+		redirect:'/goods'
+	}, //重定向
+  ],
+  linkActiveClass:'active'
 })
